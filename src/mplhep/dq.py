fc90c653da0fefbef29611d7520171d3df2e0d2c
Duc M Hoang <dhoang@submit00.mit.edu>
2022-03-21 12:16:37 -0400

    add logo functionality

--- src/mplhep/dq.py
+++ src/mplhep/dq.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import inspect
 
 from matplotlib import docstring
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
 
 import mplhep
 
@@ -46,6 +48,28 @@ def label(label=None, **kwargs):
     kwargs.setdefault("exp", "DarkQuest")
     return label_base.exp_label(**kwargs)
 
+def logo(ax, loc = 'bottom right'):
+
+    #Get the positions of the plot
+    xmin, xmax, ymin, ymax = plt.axis()
+    arr_img = plt.imread('DQ.png')
+
+    imagebox = OffsetImage(arr_img, zoom=0.1, alpha=0.2)
+    imagebox.image.axes = ax
+
+    if loc == 'bottom right':
+        xy = (xmax-0.6, ymin+0.1)
+    elif loc == 'top left':
+        xy = (xmin+0.6, ymax-0.1)
+    elif isinstance(loc, list):
+        xy = loc
+    else:
+        raise TypeError('The location needs to be "bottom right", "top left" or a list')
+    
+    ab = AnnotationBbox(imagebox, xy, frameon = False)
+    ax.add_artist(ab)
+
+    #return ax
 
 # Deprecation example
 # @deprecate.deprecate("Naming convention is changing. Use ``mplhep.cms.label``.")
